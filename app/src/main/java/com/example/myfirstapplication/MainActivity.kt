1230901f3d9da3bf56c94d9021216d2d8fe45569
Björn Larsson <larsson830@gmail.com>
2023-10-13 19:16:52 +0200

    Added more options

--- app/src/main/java/com/example/myfirstapplication/MainActivity.kt
+++ app/src/main/java/com/example/myfirstapplication/MainActivity.kt
@@ -1,12 +1,21 @@
 package com.example.myfirstapplication
 
+import android.content.Context
+import android.os.Build
 import android.os.Bundle
+import android.os.VibrationEffect
+import android.os.Vibrator
+import android.os.VibratorManager
+import android.view.HapticFeedbackConstants
+import android.view.View
+import android.view.View.INVISIBLE
 import android.view.View.VISIBLE
 import android.widget.Button
 import android.widget.ImageView
 import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -28,7 +37,12 @@ class MainActivity : AppCompatActivity() {
         var timesClicked = 0
         var rainBow = "\ud83c\udf08"
         var star ="\ud83c\udf1f"
-        btnClickMe.setOnClickListener {
+
+        btnClickMe.setOnClickListener { view ->
+
+
+            view.performHapticFeedback(HapticFeedbackConstants.CONFIRM)
+
             timesClicked +=1
             greetingText.text = timesClicked.toString()
             when (timesClicked) {
@@ -37,60 +51,60 @@ class MainActivity : AppCompatActivity() {
                     rainBow += rainBow
                 }
                 10 -> {
-                    Toast.makeText(this, rainBow,Toast.LENGTH_LONG).show();
+                    Toast.makeText(this, rainBow,Toast.LENGTH_LONG).show()
                     rainBow += "\ud83c\udf08"
                 }
                 15 -> {
-                    Toast.makeText(this, rainBow,Toast.LENGTH_LONG).show();
+                    Toast.makeText(this, rainBow,Toast.LENGTH_LONG).show()
                     rainBow += "\ud83c\udf08"
                 }
                 20 -> {
-                    Toast.makeText(this, rainBow+star,Toast.LENGTH_LONG).show();
+                    Toast.makeText(this, rainBow+star,Toast.LENGTH_LONG).show()
                     oneStar.setVisibility(VISIBLE)
                     rainBow += "\ud83c\udf08"
                     star += star
                 }
                 30 -> {
-                    Toast.makeText(this, rainBow+star,Toast.LENGTH_LONG).show();
+                    Toast.makeText(this, rainBow+star,Toast.LENGTH_LONG).show()
                     twoStar.setVisibility(VISIBLE)
                     rainBow += "\ud83c\udf08"
                     star += "\ud83c\udf1f"
                 }
                 40 -> {
-                    Toast.makeText(this, rainBow+star,Toast.LENGTH_LONG).show();
+                    Toast.makeText(this, rainBow+star,Toast.LENGTH_LONG).show()
                     threeStar.setVisibility(VISIBLE)
                     rainBow += "\ud83c\udf08"
                     star += "\ud83c\udf1f"
                 }
                 50 -> {
-                    Toast.makeText(this, rainBow+star,Toast.LENGTH_LONG).show();
+                    Toast.makeText(this, rainBow+star,Toast.LENGTH_LONG).show()
                     fourStar.setVisibility(VISIBLE)
                     rainBow += "\ud83c\udf08"
                     star += "\ud83c\udf1f"
                 }
 
                 60 -> {
-                    Toast.makeText(this, rainBow+star,Toast.LENGTH_LONG).show();
+                    Toast.makeText(this, rainBow+star,Toast.LENGTH_LONG).show()
                     fiveStar.setVisibility(VISIBLE)
                     rainBow += "\ud83c\udf08"
                     star += "\ud83c\udf1f"
                 }
 
                 70 -> {
-                    Toast.makeText(this, rainBow+star,Toast.LENGTH_LONG).show();
+                    Toast.makeText(this, rainBow+star,Toast.LENGTH_LONG).show()
                     sixStar.setVisibility(VISIBLE)
                     rainBow += "\ud83c\udf08"
                     star += "\ud83c\udf1f"
                 }
 
                 80 -> {
-                    Toast.makeText(this, rainBow+star,Toast.LENGTH_LONG).show();
+                    Toast.makeText(this, rainBow+star,Toast.LENGTH_LONG).show()
                     sevenStar.setVisibility(VISIBLE)
                     rainBow += "\ud83c\udf08"
                     star += "\ud83c\udf1f"
                 }
                 90 -> {
-                    Toast.makeText(this, rainBow+star,Toast.LENGTH_LONG).show();
+                    Toast.makeText(this, rainBow+star,Toast.LENGTH_LONG).show()
                     eightStar.setVisibility(VISIBLE)
                     rainBow += "\ud83c\udf08"
                     star += "\ud83c\udf1f"
@@ -101,6 +115,16 @@ class MainActivity : AppCompatActivity() {
             timesClicked +=1
             greetingText.text = "0"
             timesClicked = 0
+            oneStar.setVisibility(INVISIBLE)
+            twoStar.setVisibility(INVISIBLE)
+            threeStar.setVisibility(INVISIBLE)
+            fourStar.setVisibility(INVISIBLE)
+            fiveStar.setVisibility(INVISIBLE)
+            sixStar.setVisibility(INVISIBLE)
+            sevenStar.setVisibility(INVISIBLE)
+            eightStar.setVisibility(INVISIBLE)
+            rainBow = "\ud83c\udf08"
+            star ="\ud83c\udf1f"
 
 
         }
